907ac1a1254bb03695ab6e09162721bcb4348ff0
runjaj <javier.arantegui@icloud.com>
2022-12-02 09:50:09 +0100

    Sección: Obtención de la curva OC

--- Muestreo por atributos.jl
+++ Muestreo por atributos.jl
@@ -92,6 +92,7 @@ $(aside(tip(md"``d``: Número de unidades no conformes de la muestra.")))
 # ╔═╡ bd53f4d6-0852-4d29-98a9-2005e42d4692
 # Ejemplo intro
 md"""
+---
 !!! note "Ejemplo: ¿Dónde están las unidades no conformes?"
 	
 _N_ = $(@bind N_intro Slider(100:1:500, default=150, show_value=true)) | Núm. de unidades no conformes = $(@bind d_intro Slider(0:60, show_value=true))
@@ -132,6 +133,8 @@ begin
 	 $(show_intro)
 
 	 ``p`` = $(round(p_intro; digits=3)) | ``d/n``= $(round(muest_intro.d/n_intro; digits=3))
+	
+	---
 	"""
 end
 
@@ -142,8 +145,207 @@ Idealmente ``p`` y ``d/n`` deberían ser iguales, pero comprobamos que no siempr
 Es necesario que estudiemos el problema con más detalle para encontrar un valor de ``n`` que nos permita unos resultados con unas garantías aceptables pero que no disparen los costes.
 """
 
+# ╔═╡ 48265e3f-071c-4813-ba18-a67ff1e3eb38
+cm"""
+## Creación del plan de muestreo
+
+Como hemos visto un plan de muestreo consta de:
+
+* ``n``, el tamaño de muestra, y,
+* ``c``, el criterio de aceptación, que es el número máximo aceptable de unidades no conformes en la muestra.
+
+El lote será aceptable si ``d \le c``.
+
+Veamos un ejemplo de apliación del muestreo:
+
+---
+!!! note "Ejemplo de la apliación de un plan de muestreo"
+
+Seleccionar las características del lote y del plan de muestreo. Al modificar cualquiera de los valores, se realiza la simulación. Para repetirla sin cambiar los valores, solo hay que pulsarl el botón "Repetir la simulación".
+"""
+
+# ╔═╡ 57fbc946-c6c9-4a89-87ab-067f6bbea4e6
+# Ejemplo: apl
+md"""
+**Características del lote:**
+
+_N_ = $(@bind N_apl Slider(100:1000, show_value=true, default=500)) | 
+_p_ = $(@bind p_apl Slider(0.:.001:0.2, show_value=true, default=0.01))
+
+**Muestra:**
+
+_n_ = $(@bind n_apl Slider(0:1000, show_value=true, default=100)) | _c_ = $(@bind c_apl Slider(0:30, show_value=true, default=0))
+
+$(@bind go_apl Button("Repetir al simulación"))
+"""
+
+# ╔═╡ 0295d3b5-0808-4442-969a-71b02ffbc777
+begin
+	go_apl
+	
+	plan_apl = Plan(n_apl, c_apl, false)
+	md"""
+	Se **$(simul_muestreo(plan_apl, Int(floor(p_apl*N_apl)), N_apl, 1) == 1 ? "acepta" : "rechaza")** el lote.
+	
+	---
+	"""
+end
+
+# ╔═╡ 24bffeb8-8ddf-44ab-ae2b-09a7c9782bbd
+md"""
+Comprobamos hay situaciones en las que prácticamente siempre se acepta el lote. Esto se produce cuando:
+1. El nivel de calidad es muy bueno, es decir, valores de fracción de unidades no conformes $p$ bajos, es decir, pocas unidades no conformes en el lote
+2. Planes de muestreo poco exigentes, ya sea porque el tamaño de muestra $n$ bajos o por criterios de aceptación $c$ demasiado elevados.
+
+También tenemos la situación contraria, en la que los lotes se rechazan prácticamente todas las veces. Viendo los casos en los que domina la aceptación, es sencillo entender cuando se da la situación contraria.
+
+Existen situaciones intermedias en los que la aceptación y el rechazo se da en un número simular.
+
+El resultado es que tenemos una incertidumbre cuando se realiza un muestreo. Es inevitable ya que estamos trabajando con información incompleta. Debemos estudiar mejor este problema para poder encontrar una solución satisfactoria.
+
+---
+!!! note "Ejemplo: Muchos muestreos"
+
+En este caso, en lugar de realizar un solo muestreo vamos a realizar varios muestreos de un mismo lote y contaremos cuantas veces se acepta el lote.
+"""
+
+# ╔═╡ 9f95f89a-0f3d-445d-8991-f69961fca35f
+# Ejemplo: simul
+md"""
+**Características del lote:**
+
+_N_ = $(@bind N_simul Slider(100:1000, show_value=true, default=800)) | 
+_p_ = $(@bind p_simul Slider(0.:.001:0.2, show_value=true, default=0.016))
+
+**Muestra:**
+
+_n_ = $(@bind n_simul Slider(0:1000, show_value=true, default=100)) | _c_ = $(@bind c_simul Slider(0:30, show_value=true, default=1))
+
+Número de simulaciones: $(@bind num_simul Select([1, 10, 100, 1000, 10_000, 100_000], default=100)) | $(@bind go_simul Button("Repetir"))
+
+**Resultado de la simulación:**
+"""
+
+# ╔═╡ 56b21ea7-d015-4f5b-91ed-b8db6bc1b271
+begin
+	go_simul
+	
+	
+	plan_simul = Plan(n_simul, c_simul, false)
+	acep_simul = Int(round(simul_muestreo(plan_simul, Int(floor(p_simul*N_simul)), N_simul, num_simul)*num_simul))
+	md"""
+	Se aceptan | Se rechazan | Total
+	:---------:|:-----------:|:------:
+	$acep_simul|$(num_simul-acep_simul) | $num_simul
+
+	---
+	"""
+end
+
+# ╔═╡ 5a43c763-d972-41e2-8f5a-161c15b3587b
+cm"""
+Podemos calcular la probabilidad de aceptar un lote ``P_a`` a partir de los datos anteriores:
+	
+```math
+P_a = \frac{\text{Núm. de lotes aceptados}}{\text{Número total de lotes muestreados}}
+```
+$(aside(tip(md"``P_a``: Probabilidad de aceptar un lote con un nivel de calidad ``p``.")))
+
+Para la simulación anterior, encontramos que:
+
+``P_a`` = $(acep_simul)/$num_simul = $(round(acep_simul/num_simul;digits=3))
+"""
+
+
+# ╔═╡ df6efd4c-764a-4955-a74b-e482ec4ce79f
+md"""
+## Obtención de la curva característica de operación
+
+La probabilidad de aceptación nos permite saber si un lote con un cierto nivel de calidad será más probable que sea rechazado o aceptado. Idealmente deseamos que los lotes con un nivel de calidad adecuado se acepten siempre y si el nivel de calidad del lote es peor que el objetivo de calidad, se rechacen.
+
+Podemos representar de manera gráfica el razonamiento anterior representando $P_a$ en función de $p$. Esta gráfica se denomina **curva característica de operación**.
+"""
+
+# ╔═╡ cc70fc39-6884-485a-b1c5-c73445d05f39
+md"""
+Si el tamano de muestra coincide con el tamaño del lote no existe incertidumbre, ya que inspeccionamos todas las unidades del lote, se conoce el nivel de calidad sin margen de duda. Esta curva característica es la curva característica ideal.
+
+$(aside(tip(md"Se puede comprobar esta afirmación fácilmente con la simulación anterior.")))
+---
+!!! note "Curva característica de operación ideal"
+"""
+
+# ╔═╡ 1e3e1c9f-cd6a-403c-b5fc-c64a4bd50e6b
+# ejemplo: ideal
+md"""
+Nivel de calidad aceptable (NCA) = $(@bind p_ideal Slider(0:.1:1, show_value=true, default=0.5))
+"""
+
+# ╔═╡ e2761ee8-6430-4d67-9e88-6f344f39a509
+begin
+	lims_ideal = [0, 1, p_ideal]
+	lims_text_ideal = [0, 1, "NCA"]
+	plot(0:.001:1, x->(x <= p_ideal ? 1 : 0), ylabel="Pₐ", xlabel="p", legend=false,
+	lw=4, fill=0, fillalpha=.2, xticks=(lims_ideal, lims_text_ideal))
+	annotate!(p_ideal/2, 0.5, "Aceptación")
+	annotate!(p_ideal+(1-p_ideal)/2, 0.5, "Rechazo")
+end
+
+# ╔═╡ 1417443b-5b5c-477d-ba65-e5a3dc4f6811
+md"""
+---
+Podemos dibujar la curva característica de operación para un plan de muestreo utilizando las simulaciones que hemos planteado más arriba. Tendremos que repetir las simulaciones para todos los valores de _p_ que queramos representar.
+
+---
+!!! note "Curva característica de operación mediante simulaciones"
+"""
+
+# ╔═╡ bed5a7a3-1742-48b4-86a1-44e1b4216839
+# ejemplo:OCsim
+md"""
+**Características del lote:**
+
+_N_ = $(@bind N_OCsim Slider(100:1000, show_value=true, default=800)) | 
+_p máx_ = $(@bind p_OCsim Slider(0.:.001:0.2, show_value=true, default=0.016))
+
+**Muestra:**
+
+_n_ = $(@bind n_OCsim Slider(0:1000, show_value=true, default=100)) | _c_ = $(@bind c_OCsim Slider(0:30, show_value=true, default=1))
+
+Número de simulaciones: $(@bind num_OCsim Select([1, 10, 100, 1000, 10_000, 100_000], default=100)) | $(@bind go_OCsim Button("Repetir"))
+
+**Resultado de la simulación:**
+"""
+
+# ╔═╡ 9da34e6b-b895-4e95-9dfd-506e0781e00a
+begin
+	go_OCsim
+	
+	plan_OCsim = Plan(n_OCsim, c_OCsim, false)
+	np_OCsim = 0:Int(round(p_OCsim*N_OCsim))
+	oc_OCsim = crear_oc(plan_OCsim, np_OCsim, N_OCsim, num_OCsim)
+	scatter(oc_OCsim.p, oc_OCsim.Pa, legend=false, xlabel="p", ylabel="Pₐ")
+end
+
+# ╔═╡ d1a20e83-954a-4a7c-ba22-223d0ed3a0da
+md"""
+---
+Hemos calculado la curva característica de operación "experimentalmente". El problema es que hemos necesitado $(num_OCsim*length(np_OCsim)) simulaciones para poder obtenerla.
+
+Este método funciona, pero  tiene que haber una manera más sencilla de obtener los datos de probabilidad de aceptación sin tener que realizar tantos cálculos.
+"""
+
+# ╔═╡ f8ab6471-7a20-4360-a306-a1c9b236751b
+md"""
+## Curvas OC a partir de las funciones de distribución
+"""
+
+# ╔═╡ 5ad473e8-e5e2-4c0a-b4a3-3f98cfabeeef
+
+
 # ╔═╡ 9da2825d-66ff-4768-b3b0-1c865f53ba06
 md"""
+---
 ---
 """
 
@@ -159,6 +361,23 @@ PlutoUI.TableOfContents()
 # ╟─bd53f4d6-0852-4d29-98a9-2005e42d4692
 # ╟─082e6af1-0db1-40c6-9447-d44daea34c61
 # ╟─c906f8a9-1752-443c-a157-223456258ce9
+# ╟─48265e3f-071c-4813-ba18-a67ff1e3eb38
+# ╟─57fbc946-c6c9-4a89-87ab-067f6bbea4e6
+# ╟─0295d3b5-0808-4442-969a-71b02ffbc777
+# ╟─24bffeb8-8ddf-44ab-ae2b-09a7c9782bbd
+# ╟─9f95f89a-0f3d-445d-8991-f69961fca35f
+# ╟─56b21ea7-d015-4f5b-91ed-b8db6bc1b271
+# ╟─5a43c763-d972-41e2-8f5a-161c15b3587b
+# ╟─df6efd4c-764a-4955-a74b-e482ec4ce79f
+# ╟─cc70fc39-6884-485a-b1c5-c73445d05f39
+# ╟─1e3e1c9f-cd6a-403c-b5fc-c64a4bd50e6b
+# ╟─e2761ee8-6430-4d67-9e88-6f344f39a509
+# ╟─1417443b-5b5c-477d-ba65-e5a3dc4f6811
+# ╟─bed5a7a3-1742-48b4-86a1-44e1b4216839
+# ╟─9da34e6b-b895-4e95-9dfd-506e0781e00a
+# ╟─d1a20e83-954a-4a7c-ba22-223d0ed3a0da
+# ╟─f8ab6471-7a20-4360-a306-a1c9b236751b
+# ╠═5ad473e8-e5e2-4c0a-b4a3-3f98cfabeeef
 # ╟─9da2825d-66ff-4768-b3b0-1c865f53ba06
 # ╠═e940f120-fbe8-481b-ba90-17d5af80fa07
 # ╠═10a95832-912a-4b4e-b165-41efd7cd6e61
